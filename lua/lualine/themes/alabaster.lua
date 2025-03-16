c496f531c6fadcae980d7f5aea1bfde4bb587088
Parker Spielvogel <paspielvogel@gmail.com>
2025-03-16 17:26:20 -0400

    Update some colors; get rid of light theme

--- lua/lualine/themes/alabaster.lua
+++ lua/lualine/themes/alabaster.lua
@@ -1,45 +1,35 @@
-local colors
-if vim.o.background == "dark" then
-    colors = {
-        color1 = "#162022",
-        color2 = "#cd974b",
-        color3 = "#cecece",
-        color4 = "#9F9F9F",
-        color5 = "#333333",
-        color6 = "#cc8bc9",
-    }
-else
-    colors = {
-        color1 = "#c9c9c9",
-        color2 = "#cb9000",
-        color3 = "#222222",
-        color4 = "#666666",
-        color5 = "#aaaaaa",
-        color6 = "#7a3e9d",
-    }
-end
+local colors = {
+	fg = "#162022",
+	fg_dark = "#cecece",
+	normal_bg = "#0083b2",
+	insert_bg = "#448c27",
+	visual_bg = "#7a3e9d",
+	replace_bg = "#cb9000",
+	color4 = "#9F9F9F",
+}
 
 return {
-    replace = {
-        a = { fg = colors.color1, bg = colors.color2, gui = "bold" },
-        b = { fg = colors.color3, bg = colors.color1 },
-    },
-    inactive = {
-        a = { fg = colors.color4, bg = colors.color1, gui = "bold" },
-        b = { fg = colors.color4, bg = colors.color1 },
-        c = { fg = colors.color4, bg = colors.color1 },
-    },
-    normal = {
-        a = { fg = colors.color3, bg = colors.color5, gui = "bold" },
-        b = { fg = colors.color3, bg = colors.color1 },
-        c = { fg = colors.color3, bg = colors.color1 },
-    },
-    visual = {
-        a = { fg = colors.color1, bg = colors.color6, gui = "bold" },
-        b = { fg = colors.color3, bg = colors.color1 },
-    },
-    insert = {
-        a = { fg = colors.color1, bg = colors.color3, gui = "bold" },
-        b = { fg = colors.color3, bg = colors.color1 },
-    },
+	replace = {
+		a = { fg = colors.fg, bg = colors.replace_bg, gui = "bold" },
+		b = { fg = colors.fg_dark, bg = colors.fg },
+		c = { fg = colors.fg_dark, bg = colors.fg },
+	},
+	inactive = {
+		a = { fg = colors.color4, bg = colors.fg, gui = "bold" },
+		b = { fg = colors.color4, bg = colors.fg },
+		c = { fg = colors.color4, bg = colors.fg },
+	},
+	normal = {
+		a = { fg = colors.fg_dark, bg = colors.normal_bg, gui = "bold" },
+		b = { fg = colors.fg_dark, bg = colors.fg },
+		c = { fg = colors.fg_dark, bg = colors.fg },
+	},
+	visual = {
+		a = { fg = colors.fg, bg = colors.visual_bg, gui = "bold" },
+		b = { fg = colors.fg_dark, bg = colors.fg },
+	},
+	insert = {
+		a = { fg = colors.fg, bg = colors.insert_bg, gui = "bold" },
+		b = { fg = colors.fg_dark, bg = colors.fg },
+	},
 }
